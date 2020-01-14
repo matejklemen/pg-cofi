2582755d4e792eb48a6b651b2e343c2bc0fa179e
Matej Klemen <matej.klemen1337@gmail.com>
2020-01-14 22:46:04 +0100

    Add bias correction for collaborative filtering

--- pg_cofi/user_based_cofi.sql
+++ pg_cofi/user_based_cofi.sql
@@ -1,5 +1,29 @@
 \c movielens;
 
+DROP TABLE IF EXISTS "AvgUserRating";
+
+CREATE TABLE "AvgUserRating" AS
+SELECT user_id, AVG(rating) as avg_rating
+FROM "Rating" 
+GROUP BY user_id;
+
+ALTER TABLE "AvgUserRating" ADD PRIMARY KEY (user_id);
+
+DROP TABLE IF EXISTS "NormalizedRating";
+
+CREATE TABLE "NormalizedRating" AS
+SELECT 
+	r.user_id AS user_id,
+	r.movie_id AS movie_id,
+	(r.rating - avgr.avg_rating) AS rating,
+	r.timestamp
+FROM 
+	"Rating" AS r JOIN 
+	"AvgUserRating" AS avgr ON 
+	avgr.user_id = r.user_id;
+
+ALTER TABLE "NormalizedRating" ADD PRIMARY KEY (user_id, movie_id);
+
 DROP TABLE IF EXISTS "CommonMovieAggregate" CASCADE;
 
 -- pre-compute similarities
@@ -8,10 +32,11 @@ SELECT
 	r1.user_id AS u1,
 	r2.user_id AS u2,
 	COUNT(*) AS N,
-	SUM(r1.rating * r2.rating) / (SQRT(SUM(r1.rating * r1.rating)) * SQRT(SUM(r2.rating * r2.rating))) AS cosine_sim
+	-- add small constant to prevent division by zero
+	SUM(r1.rating * r2.rating) / (SQRT(SUM(r1.rating * r1.rating)) * SQRT(SUM(r2.rating * r2.rating)) + 1e-10) AS cosine_sim
 FROM
-	"Rating" AS r1 JOIN
-	"Rating" AS r2 ON r1.movie_id = r2.movie_id
+	"NormalizedRating" AS r1 JOIN
+	"NormalizedRating" AS r2 ON r1.movie_id = r2.movie_id
 WHERE
 	-- store each common user once per (u1, u2) pair, i.e. (u1, u2, movie) = (u2, u1, movie)
 	r1.user_id < r2.user_id
@@ -42,7 +67,7 @@ BEGIN
 			COALESCE(SUM(agg1.cosine_sim * r1.rating), 0) AS weighted_ratings 
 		FROM 
 			(SELECT * FROM "CommonMovieAggregate" WHERE u1 = u_id) AS agg1
-			JOIN (SELECT * FROM "Rating" WHERE movie_id = m_id) AS r1
+			JOIN (SELECT * FROM "NormalizedRating" WHERE movie_id = m_id) AS r1
 			ON agg1.u2 = r1.user_id
 	),
 	sims2 AS (
@@ -51,14 +76,14 @@ BEGIN
 			COALESCE(SUM(agg2.cosine_sim * r2.rating), 0) AS weighted_ratings
 		FROM 
 			(SELECT * FROM "CommonMovieAggregate" WHERE u2 = u_id) AS agg2
-			JOIN (SELECT * FROM "Rating" WHERE movie_id = m_id) AS r2
+			JOIN (SELECT * FROM "NormalizedRating" WHERE movie_id = m_id) AS r2
 			ON agg2.u1 = r2.user_id
 	)
 	-- weighted sum of ratings / sum of similarities
 	SELECT ((SELECT weighted_ratings FROM sims1) + (SELECT weighted_ratings FROM sims2)) / 
 			((SELECT sum_of_sims FROM sims1) + (SELECT sum_of_sims FROM sims2)) INTO result;
 
-	RETURN result;
+	RETURN result + (SELECT avg_rating FROM "AvgUserRating" WHERE user_id = u_id);
 END;
 $$
 LANGUAGE PLPGSQL;
